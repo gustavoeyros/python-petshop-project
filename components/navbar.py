6782956347da08ecb6a56b2e435739a5feea4702
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-17 11:12:00 -0300

    feat: circles styled

--- components/navbar.py
+++ components/navbar.py
@@ -1,37 +1,30 @@
 from tkinter import *
-import ttkbootstrap as tt
 
-root = Tk()
-
-root.title("Petshop Dog's")
-root.config(background='#FFFFFF')
-root.resizable(False, False)
-root.maxsize(width=1280, height=720)
-
-root.minsize(width=1280, height=720)
 
 # default colors
 font_default = "Inter 13 bold"
 
 
-# home_option
-home_option__navbar = Label(
-    root, text="Home", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
-home_option__navbar.grid(row=0, column=0)
+class Navbar(Frame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-# signup_option
-signup_option__navbar = Label(
-    root, text="Sign up", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
-signup_option__navbar.grid(row=0, column=1)
+        # home_option
+        home_option__navbar = Label(
+            self, text="Home", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
+        home_option__navbar.grid(row=0, column=0)
 
-# animal_rgister_option
-animal_register_option__navbar = Label(
-    root, text="Animal register", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
-animal_register_option__navbar.grid(row=0, column=2)
+        # signup_option
+        signup_option__navbar = Label(
+            self, text="Sign up", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
+        signup_option__navbar.grid(row=0, column=1)
 
-# services_option
-services_option__navbar = Label(
-    root, text="Services", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
-services_option__navbar.grid(row=0, column=3)
+        # animal_rgister_option
+        animal_register_option__navbar = Label(
+            self, text="Animal register", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
+        animal_register_option__navbar.grid(row=0, column=2)
 
-root.mainloop()
+        # services_option
+        services_option__navbar = Label(
+            self, text="Services", font=font_default, fg="#18191F", bg="#FFFFFF", padx=10)
+        services_option__navbar.grid(row=0, column=3)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import ttkbootstrap as tt
+from components.navbar import Navbar
+from PIL import Image, ImageTk
 
 root = Tk()
 
@@ -7,8 +8,30 @@ root.title("Petshop Dog's")
 root.config(background='#FFFFFF')
 root.resizable(False, False)
 root.maxsize(width=1280, height=720)
-
 root.minsize(width=1280, height=720)
 
+
+# Menu - Navbar
+navbarMenu = Navbar(master=root, width=250)
+navbarMenu.grid(row=0, column=0, padx=150)
+
+# Purple - Circle
+circle_image = Image.open('assets\\circle_home.png')
+circle_convert_tk = ImageTk.PhotoImage(circle_image)
+lbl_image = Label(root, image=circle_convert_tk, bg="#FFFFFF")
+lbl_image.place(x=800, y=-80)
+
+# Orange - Circle
+orange_image = Image.open('assets\\orange_circle.png')
+orange_convert_tk = ImageTk.PhotoImage(orange_image)
+lbl_image = Label(root, image=orange_convert_tk, bg="#FFFFFF")
+lbl_image.place(x=700, y=300)
+
+
+# Turquoise - Circle
+turquoise_image = Image.open('assets\\turquoise_circle.png')
+turquoise_convert_tk = ImageTk.PhotoImage(turquoise_image)
+lbl_image = Label(root, image=turquoise_convert_tk, bg="#FFFFFF")
+lbl_image.place(x=885, y=550)
 
 root.mainloop()
