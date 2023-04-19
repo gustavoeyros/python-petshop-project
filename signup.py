c662bde5afb7ff166eddc749120f44bf95d4d519
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-19 13:41:28 -0300

    feat: button to sign up and change entrys coordinates

--- signup.py
+++ signup.py
@@ -22,22 +22,27 @@ def signUp():
     # Form
 
     # Name
-    userEntry = Entry(root, width=40, bg="white")
-    userEntry.place(x=550, y=180)
+    userEntry = Entry(root, width=55, bg="white")
+    userEntry.place(x=550, y=320)
     userLabel = Label(root, text="Name: ", font="Inter 10 bold",)
-    userLabel.place(x=495, y=177)
+    userLabel.place(x=495, y=317)
 
     # Email
-    emailEntry = Entry(root, width=40, bg="white")
-    emailEntry.place(x=550, y=210)
+    emailEntry = Entry(root, width=55, bg="white")
+    emailEntry.place(x=550, y=350)
     emailLabel = Label(root, text="E-mail: ", font="Inter 10 bold",)
-    emailLabel.place(x=495, y=207)
+    emailLabel.place(x=495, y=347)
 
     # Password
-    passwordEntry = Entry(root, width=40, bg="white")
-    passwordEntry.place(x=570, y=240)
+    passwordEntry = Entry(root, width=52, bg="white")
+    passwordEntry.place(x=570, y=380)
     passwordLabel = Label(root, text="Password: ", font="Inter 10 bold",)
-    passwordLabel.place(x=495, y=237)
+    passwordLabel.place(x=495, y=377)
+
+    # Button
+    btnSignUp = Button(root, text="Sign Up",
+                       font="Inter 10 bold", fg="white", bg="#8C30F5", border=5, background="#8C30F5", bd=0, width=48)
+    btnSignUp.place(x=495, y=435)
 
     root.mainloop()
 
